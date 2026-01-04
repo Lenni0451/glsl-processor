e0a5ff6a71d2d0e28c3b435bcf2e642f4ad6164a
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2026-01-04 19:26:30 +0100

    Fixed injection points

--- src/main/java/io/github/ocelot/glslprocessor/api/GlslInjectionPoint.java
+++ src/main/java/io/github/ocelot/glslprocessor/api/GlslInjectionPoint.java
@@ -3,7 +3,7 @@ package io.github.ocelot.glslprocessor.api;
 import io.github.ocelot.glslprocessor.api.node.GlslTree;
 
 /**
- * Specific locations inside a {@link GlslTree} nodes can be added to.
+ * Reference locations inside a {@link GlslTree} nodes can be added to.
  *
  * @author Ocelot
  * @since 1.0.0
@@ -12,9 +12,9 @@ public enum GlslInjectionPoint {
 
     BEFORE_DECLARATIONS,
     AFTER_DECLARATIONS,
+    BEFORE_FUNCTIONS,
+    AFTER_FUNCTIONS,
     BEFORE_MAIN,
     AFTER_MAIN,
-    BEFORE_FUNCTIONS,
-    AFTER_FUNCTIONS
 
 }
--- src/main/java/io/github/ocelot/glslprocessor/api/node/GlslNodeList.java
+++ src/main/java/io/github/ocelot/glslprocessor/api/node/GlslNodeList.java
@@ -29,60 +29,81 @@ public class GlslNodeList implements List<GlslNode> {
         this.nodes = new ArrayList<>(nodes);
     }
 
-    public void add(final GlslInjectionPoint point, final GlslNode element) {
-        this.nodes.add(this.getInjectionIndex(point), element);
+    public void add(final GlslInjectionPoint referencePoint, final GlslNode element) {
+        this.nodes.add(this.getInjectionIndex(referencePoint), element);
     }
 
-    public void addAll(final GlslInjectionPoint point, final Collection<GlslNode> nodes) {
-        this.nodes.addAll(this.getInjectionIndex(point), nodes);
+    public void addAll(final GlslInjectionPoint referencePoint, final Collection<GlslNode> nodes) {
+        this.nodes.addAll(this.getInjectionIndex(referencePoint), nodes);
     }
 
-    private int getInjectionIndex(final GlslInjectionPoint point) {
-        switch (point) {
+    private int getInjectionIndex(final GlslInjectionPoint referencePoint) {
+        switch (referencePoint) {
             case BEFORE_DECLARATIONS -> {
                 for (int i = 0; i < this.nodes.size(); i++) {
-                    if (!(this.nodes.get(i) instanceof GlslFunctionNode)) {
+                    GlslNode node = this.nodes.get(i);
+                    if (this.isDeclarationNode(node) || this.isFunctionNode(node)) {
                         return i;
                     }
                 }
             }
             case AFTER_DECLARATIONS -> {
                 for (int i = 0; i < this.nodes.size(); i++) {
-                    if (this.nodes.get(i) instanceof GlslFunctionNode) {
+                    GlslNode node = this.nodes.get(i);
+                    if (!this.isDeclarationNode(node)) {
                         return i;
-                    }
-                }
-            }
-            case BEFORE_MAIN -> {
-                for (int i = 0; i < this.nodes.size(); i++) {
-                    if (this.nodes.get(i) instanceof GlslFunctionNode functionNode && "main".equals(functionNode.getHeader().getName())) {
-                        return i;
-                    }
-                }
-            }
-            case AFTER_MAIN -> {
-                for (int i = 0; i < this.nodes.size(); i++) {
-                    if (this.nodes.get(i) instanceof GlslFunctionNode functionNode && "main".equals(functionNode.getHeader().getName())) {
-                        return i + 1;
                     }
                 }
             }
             case BEFORE_FUNCTIONS -> {
                 for (int i = 0; i < this.nodes.size(); i++) {
-                    if (this.nodes.get(i) instanceof GlslFunctionNode) {
+                    GlslNode node = this.nodes.get(i);
+                    if (this.isFunctionNode(node)) {
                         return i;
                     }
                 }
             }
             case AFTER_FUNCTIONS -> {
                 for (int i = this.nodes.size() - 1; i >= 0; i--) {
-                    if (this.nodes.get(i) instanceof GlslFunctionNode) {
+                    GlslNode node = this.nodes.get(i);
+                    if (this.isFunctionNode(node)) {
+                        return i + 1;
+                    }
+                }
+            }
+            case BEFORE_MAIN -> {
+                for (int i = 0; i < this.nodes.size(); i++) {
+                    GlslNode node = this.nodes.get(i);
+                    if (this.isMain(node)) {
+                        return i;
+                    }
+                }
+            }
+            case AFTER_MAIN -> {
+                for (int i = 0; i < this.nodes.size(); i++) {
+                    GlslNode node = this.nodes.get(i);
+                    if (this.isMain(node)) {
                         return i + 1;
                     }
                 }
             }
         }
         return 0;
+    }
+
+    private boolean isDeclarationNode(final GlslNode node) {
+        if (node instanceof GlslFunctionNode functionNode) {
+            return functionNode.getBody() == null;
+        }
+        return true;
+    }
+
+    private boolean isFunctionNode(final GlslNode node) {
+        return node instanceof GlslFunctionNode functionNode && functionNode.getBody() != null;
+    }
+
+    private boolean isMain(final GlslNode node) {
+        return node instanceof GlslFunctionNode functionNode && "main".equals(functionNode.getName()) && functionNode.getBody() != null;
     }
 
 }
